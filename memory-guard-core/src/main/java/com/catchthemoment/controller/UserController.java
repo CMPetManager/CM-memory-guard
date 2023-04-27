1235903b95df410196ae1e01ef54f3d8f453f4f5
Vlad Marmuz <marmuzvladislav@gmail.com>
2023-04-27 15:21:30 +0300

    add password validation

--- memory-guard-core/src/main/java/com/catchthemoment/controller/UserController.java
+++ memory-guard-core/src/main/java/com/catchthemoment/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.catchthemoment.controller;
 
 import com.catchthemoment.entity.User;
+import com.catchthemoment.exception.ApplicationErrorEnum;
+import com.catchthemoment.exception.ServiceProcessingException;
 import com.catchthemoment.model.UpdatePassword;
 import com.catchthemoment.service.UserResetPasswordService;
 import com.catchthemoment.service.UserService;
+import com.catchthemoment.validation.UpdatePasswordValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +21,15 @@ public class UserController implements UserControllerApiDelegate {
 
     private final UserResetPasswordService userResetPasswordService;
     private final UserService userService;
+    private final UpdatePasswordValidator validator;
 
     @Override
     public ResponseEntity<String> updateEmail(Long userId, UpdatePassword updatePassword) throws Exception {
+        if(!validator.isValid(updatePassword)){
+            log.error("*** Password didn't pass validation ***");
+            throw new ServiceProcessingException(ApplicationErrorEnum.INCORRECT_INPUT.getCode(),
+                    ApplicationErrorEnum.INCORRECT_INPUT.getMessage());
+        }
         User currentUser = userService.getById(userId);
         log.info("*** Received an update password request with email: {} ***", currentUser.getEmail());
         userResetPasswordService.updatePassword(currentUser, updatePassword.getPassword());
