a9371e5563697fbbe550064c07203984335de132
Ivanshelemeh <neo041990@mail.ru>
2023-05-08 23:56:14 +0300

    added dockerfile for resolving deploy

--- memory-guard-core/src/main/java/com/catchthemoment/CatchTheMomentApplication.java
+++ memory-guard-core/src/main/java/com/catchthemoment/CatchTheMomentApplication.java
@@ -2,12 +2,10 @@ package com.catchthemoment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@Profile("prod")
 public class CatchTheMomentApplication {
 
     public static void main(String[] args) {
